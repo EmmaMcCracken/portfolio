4e7d5b1d335dcae2ef03cf07cf7c5fe79392f7b8
Emma McCracken <emma.mccracken14@gmail.com>
2023-12-02 19:31:00 +0000

    attempt to revert CSS changes

--- src/portfolio.js
+++ src/portfolio.js
@@ -6,27 +6,27 @@
 
 const about = {
   // all the properties are optional - can be left empty or deleted
-  name: 'Emma McCracken',
+  name: 'Emma Oatley',
   role: 'Software Engineer',
   description:
     'Open-minded mathematician turned coder who loves learning. Looking for challenges to solve in software, whether they be in the back-end or the front-end.  ',
   resume:
     'https://drive.google.com/file/d/1sHunbCCLHocHF73JQnAec3wFj77nMSFk/view?usp=sharing',
   social: {
-    linkedin: 'http://linkedin.com/in/emma-mccracken1',
-    github: 'https://github.com/EmmaMcCracken',
+    linkedin: 'http://linkedin.com/in/eoatley',
+    github: 'https://github.com/eoatley',
   },
 }
 
 const projects = [
-  // projects can be added an removed
+  // projects can be added and removed
   // if there are no projects, Projects section won't show up
   {
     name: 'Decision Making Helper App',
     description:
       'Add or remove choices, attributes and their associated weightings. The app will calculate your best choice.',
     stack: ['TypeScript', 'React', 'React Flow'],
-    sourceCode: 'https://github.com/EmmaMcCracken/decision-making-helper',
+    sourceCode: 'https://github.com/eoatley/decision-making-helper',
     livePreview: 'https://help-me-choose-please.netlify.app/',
   },
   {
@@ -41,41 +41,38 @@ const projects = [
     description:
       'An app where you can view episodes from a TV-Show of your choice.',
     stack: ['TypeScript', 'React'],
-    sourceCode: 'https://github.com/EmmaMcCracken/TV-Shows-App',
+    sourceCode: 'https://github.com/eoatley/TV-Shows-App',
     livePreview: 'https://emmastvshows.netlify.app/',
   },
-  {
-    name: "Resource Catalogue (Willy Wonka's Resource Factory)",
-    description: 'A tool to add, rate, and save resources. Group project.',
-    stack: ['TypeScript', 'React', 'Node.js', 'Express', 'PostgreSQL'],
-    sourceCode: 'https://github.com/c3c3-academy/resource-catalogue-fe',
-    livePreview: 'https://c3c3-resource-catalogue.netlify.app/',
-  },
+  // {
+  //   name: "Resource Catalogue (Willy Wonka's Resource Factory)",
+  //   description: 'A tool to add, rate, and save resources. Group project.',
+  //   stack: ['TypeScript', 'React', 'Node.js', 'Express', 'PostgreSQL'],
+  //   sourceCode: 'https://github.com/c3c3-academy/resource-catalogue-fe',
+  //   livePreview: 'https://c3c3-resource-catalogue.netlify.app/',
+  // },
 ]
 
 const skills = [
   // skills can be added or removed
   // if there are no skills, Skills section won't show up
-
-  'Express',
-  'node.js',
-  'PostgreSQL',
-  'JavaScript',
-  'TypeScript',
-  'Jest',
-  'React',
-  'HTML',
-
-  'Git',
-  'CI/CD',
-  'CSS',
-
-  'Bootstrap',
+  // 'Express',
+  // 'Node.js',
+  // 'PostgreSQL',
+  // 'JavaScript',
+  // 'TypeScript',
+  // 'Jest',
+  // 'React',
+  // 'HTML',
+  // 'Git',
+  // 'CI/CD',
+  // 'CSS',
+  // 'Bootstrap',
 ]
 
 const contact = {
   // email is optional - if left empty Contact section won't show up
-  email: 'emma.mccracken14@gmail.com',
+  email: 'emma@oatley.ie',
 }
 
 export { about, projects, skills, contact }
